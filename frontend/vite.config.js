d31a4a9a16467e897cd78c74d242675114206234
Elijah Abolaji <tyabolaji@gmail.com>
2025-08-28 22:08:46 +0100

    Base updated for both local and remote test

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? "lnt" : "/",
   plugins: [react(), tailwindcss()],
-})
+}))
