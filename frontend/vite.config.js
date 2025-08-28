0ca4130cc245233f26407108ed1842ca18fea28e
Elijah Abolaji <tyabolaji@gmail.com>
2025-08-28 22:23:37 +0100

    base directory removed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? "lnt" : "/",
+export default defineConfig(() => ({
+  // base: mode === 'production' ? "lnt" : "/",
   plugins: [react(), tailwindcss()],
 }))
